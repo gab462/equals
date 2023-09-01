278722fccd59f9405cacf03110e521136105680c
gab462 <gustavoaraujob1+1@gmail.com>
2023-09-01 12:24:42 -0300

    Improve build file

--- build.cc
+++ build.cc
@@ -1,14 +1,40 @@
 #include "data.hh"
 #include "build.hh"
-
-// cppcheck examples.cc --enable=all --suppress=noExplicitConstructor  --suppress=useStlAlgorithm --suppress=missingIncludeSystem
-// valgrind ./a.out one,two -o three
+#include "arguments.hh"
 
 auto
-main () -> int {
-  eq::build_config build {"examples.cc"};
+main (int argc, char** argv) -> int {
+  eq::argument_parser args;
 
-  build.run();
+  auto& type = args.string_option ('t', "build");
+
+  args.parse (argc, argv);
+
+  if (type == "build") {
+    eq::build_config build {"examples.cc"};
+
+    build.run ();
+  }
+
+  if (type == "self") {
+    eq::build_config build {"build.cc"};
+
+    build.flags.append ("-o");
+    build.flags.append ("build");
+
+    build.run ();
+  }
+
+  if (type == "check")
+    eq::command {
+      "cppcheck", "examples.cc", "--enable=all",
+      "--suppress=noExplicitConstructor",
+      "--suppress=useStlAlgorithm",
+      "--suppress=missingIncludeSystem"
+    }.execute ();
+
+  if (type == "valgrind")
+    eq::command {"valgrind", "./a.out", "one,two", "-o", "three"}.execute();
 
   return 0;
 }
